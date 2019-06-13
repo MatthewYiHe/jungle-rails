d442409d6dca3a397fda5fbef2ad004749215ee5
MatthewYiHe <matthewhe1@hotmail.com>
2019-06-13 20:29:52 +0000

    add item in to order screen

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,9 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    @line_items = @order.line_items.all.map do |item|
+      { product: item.product, quantity: item.quantity }
+    end
   end
 
   def create
